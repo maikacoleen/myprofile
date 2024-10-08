48c93a32524e8b1f091589b6de7cb0708edf118a
Maika Coleen Rabenitas <maikacoleen1205@gmail.com>
2024-10-08 14:02:54 -0700

    About component added

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+
+    return(
+        <section>
+            <div className="mx-16">
+            <h1 className="text-yellowtext text-3xl font-bold">About.</h1>
+            <p className="text-white text-lg">Hello everyone! I’m a recent Computer Science graduate from the University of Victoria, passionate about expanding my skills across various areas of tech. While I’ve been focusing on web development—working with React, JavaScript, and modern UI libraries—I also enjoy improving my knowledge in backend systems, database design, and security practices. I love diving into new technologies and frameworks across different fields, always striving to reach a solid level of competency.</p>
+            </div>
+        </section>
+
+    );
+
+};
+
+export default About;
