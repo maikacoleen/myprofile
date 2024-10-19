feda259f639bf77221f4ace744c891e19ddf1cd7
Maika Coleen Rabenitas <maikacoleen1205@gmail.com>
2024-10-19 00:52:51 -0700

    configuration for hosting with Github

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://maikacoleen.github.io',
+  base: '/myprofile',
 });
